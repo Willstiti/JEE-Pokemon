73acc015a5cfd33e5d8181bc5a82441f18eb42c2
Willstiti <william.deneys@gmail.com>
2024-10-22 14:39:02 +0200

    Push

--- src/main/java/fr/efrei/pokemon/repositories/PokemonRepository.java
+++ src/main/java/fr/efrei/pokemon/repositories/PokemonRepository.java
@@ -1,8 +1,9 @@
 package fr.efrei.pokemon.repositories;
 
-import fr.efrei.pokemon.models.Pokemon;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import fr.efrei.pokemon.models.Pokemon;
 
 @Repository
 public interface PokemonRepository extends JpaRepository<Pokemon, String> { }
